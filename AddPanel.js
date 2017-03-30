359239c92f1023a5f01b5c7c1e4b0607238473e7
Francisco <a40637francisco@users.noreply.github.com>
2017-03-30 22:29:54 +0100

    Update AddPanel.js

--- AddPanel.js
+++ AddPanel.js
@@ -1,13 +1,4 @@
 
-function AddPanel(){
-  $("add-panel").append(
-    ImageInput(),
-    ExerciseInput(),
-    NotesInput(),
-    TimeInput(),
-  );
-} AddPanel();
-
 function LOL(){
   Console.log("aahaha");
 
